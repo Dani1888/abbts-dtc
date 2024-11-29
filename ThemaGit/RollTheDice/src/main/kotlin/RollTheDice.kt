ad36a4f2b602b31087ff4fd22e131579d52211ad
Dani1888 <bergerdani@windowslive.com>
2024-11-29 18:21:01 +0100

    // Todo: Den Fehler im Programm finden und beheben         // Todo: Den Spieler nach dem Namen fragen

--- ThemaGit/RollTheDice/src/main/kotlin/RollTheDice.kt
+++ ThemaGit/RollTheDice/src/main/kotlin/RollTheDice.kt
@@ -1,4 +1,5 @@
 import kotlin.random.Random
+import kotlin.random.nextInt
 
 /**
  * Simuliert ein einfaches Würfelspiel zwischen dem
@@ -9,21 +10,28 @@ import kotlin.random.Random
  */
 
 fun main() {
+    while (true) {
+        val die = Random
 
-    val die = Random
+        val playerScore = die.nextInt(1..6)
+        val computerScore = die.nextInt(1..6)
 
-    val playerScore = die.nextInt(7)
-    val computerScore = die.nextInt(7)
+        // Todo: Den Fehler im Programm finden und beheben
+        // Todo: Den Spieler nach dem Namen fragen
+        // Todo: Solange spielen bis Spieler abbricht
+        // Todo: Ausgeben, wer mehr Runden gewonnen hat
 
-    // Todo: Den Fehler im Programm finden und beheben
-    // Todo: Den Spieler nach dem Namen fragen
-    // Todo: Solange spielen bis Spieler abbricht
-    // Todo: Ausgeben, wer mehr Runden gewonnen hat
 
-    println("Du würfelst: $playerScore  --  Computer würfelt: $computerScore")
-    when {
-        playerScore > computerScore -> println("Du gewinnst")
-        playerScore < computerScore -> println("Der Computer gewinnt")
-        else -> println("Unentschieden")
+        print("Gib deinen Namen ein: ")
+        val name = readLine() ?: "" // Read user input and handle potential null
+        println("Name = $name: ")
+
+
+        println("Du würfelst: $playerScore  --  Computer würfelt: $computerScore")
+        when {
+            playerScore > computerScore -> println("Du gewinnst")
+            playerScore < computerScore -> println("Der Computer gewinnt")
+            else -> println("Unentschieden")
+        }
     }
 }
